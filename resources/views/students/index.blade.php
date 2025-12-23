1ab697fe74dc5280adc4774471b4c00c7a136a2d
ngovkimsouheng <kokocorona9@gmail.com>
2025-12-23 22:05:12 +0700

    use @yield to make the dynamic

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -1,19 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Student Index</title>
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-
-</head>
-
-<body>
-
-    <div class="container">
-        <h1 class="display-1">Student Index</h1>
+        @extends('layouts.myapp')
+        @section('title','Stident List')
+        @section('content')
         <a href="{{ route('students.create') }}" class="btn btn-warning my-2">CREATE NEW</a>
         <table border="1px" class="table table-hover">
             <thead class="table-primary">
@@ -42,34 +29,4 @@
             </tbody>
         </table>
         {{ $students->links('pagination::bootstrap-5') }}
-    </div>
-    <!-- sweetalert2-->
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script>
-        document.querySelectorAll('.btn-delete').forEach(button => {
-            button.addEventListener('click', function(e) {
-                let form = this.closest('form');
-                Swal.fire({
-                    title: "Are you sure?"
-                    , text: "This item will be permanently deleted."
-                    , icon: "warning"
-                    , showCancelButton: true
-                    , confirmButtonColor: "#d33"
-                    , cancelButtonColor: "#3085d6"
-                    , confirmButtonText: "Yes, delete!"
-                }).then((result) => {
-                    if (result.isConfirmed) {
-                        form.submit();
-                    }
-                });
-            });
-        });
-
-    </script>
-    <!-- end of sweetalert2-->
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous">
-    </script>
-</body>
-
-</html>
+        @endsection
